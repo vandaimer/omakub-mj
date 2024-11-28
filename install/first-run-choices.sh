35cabe0c50b79acbb6582a1a5fc4f8b799a5680a
Luiz Filipe <moresco.luiz@gmail.com>
2024-11-28 13:42:49 +0100

    Update SELECTED_LANGUAGES
    Remove Ruby on Rails
    Add Python and Go

--- install/first-run-choices.sh
+++ install/first-run-choices.sh
@@ -3,7 +3,7 @@ DEFAULT_OPTIONAL_APPS='Bitwarden,Spotify'
 export OMAKUB_FIRST_RUN_OPTIONAL_APPS=$(gum choose "${OPTIONAL_APPS[@]}" --no-limit --selected $DEFAULT_OPTIONAL_APPS --height 7 --header "Select optional apps" | tr ' ' '-')
 
 AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "PHP" "Python" "Elixir" "Rust" "Java")
-SELECTED_LANGUAGES="Ruby on Rails","Node.js"
+SELECTED_LANGUAGES="Node.js","Python","Go"
 export OMAKUB_FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
 AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
