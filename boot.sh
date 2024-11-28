79527b5d1e96c0aeff1844a88ac7c02528b934b4
Luiz Filipe <moresco.luiz@gmail.com>
2024-11-28 13:29:20 +0100

    Check if the user wants a fresh instalation
    With that, the script avoids cleaning up every time

--- boot.sh
+++ boot.sh
@@ -14,13 +14,31 @@ echo -e "\nBegin installation (or abort with ctrl+c)..."
 command -v yay >/dev/null 2>&1 || sudo pacman -S --noconfirm yay git
 yay -Syyuu --noconfirm
 
-echo "Cloning Omakub..."
-rm -rf ~/.local/share/omakub
-git clone https://github.com/akitaonrails/omakub.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
-  cd ~/.local/share/omakub
-  git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-  cd -
+fresh_instalation() {
+  echo "Fresh installation. Cloning Omakub..."
+  rm -rf ~/.local/share/omakub
+  git clone https://github.com/akitaonrails/omakub.git ~/.local/share/omakub >/dev/null
+  if [[ $OMAKUB_REF != "master" ]]; then
+    cd ~/.local/share/omakub
+    git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+    cd -
+  fi
+}
+
+DIR=~/.local/share/omakub
+
+if [[ -d "$DIR" ]]; then
+  echo "Instalation found!"
+  read -n 1 -p "Do you want to have a fresh instalation? [Y/N] (enter/empty/default: N): " yn
+  echo "" # Add a new line after reading answer
+  case $yn in
+  [yY]*)
+    fresh_instalation
+    break
+    ;;
+  esac
+else
+  fresh_instalation
 fi
 
 echo "Installation starting..."
